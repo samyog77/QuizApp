0215963aaba2c06745664dfe99b20c5fe67814cc
Samyog <samyog.dc@gmail.com>
2024-07-09 13:24:45 +1000

    first commit 7/9/2024

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import quizLogo from "../assets/quiz-logo.png"
+export default function (){
+    return(
+        
+        <header>
+        <img src={quizLogo} alt="Quiz Logo" className=""/>
+        <h1>REACT QUIZ</h1>
+        </header>
+
+        
+        
+  
+        
+
+       
+    );
+}
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
+export default function Quiz(){
+    const [userAnswers, setUserAnswers] =useState([]);
+    const activeQuestionIndex = userAnswers.length;
+    
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length; 
+    
+    
+
+    function handleSelectAnswer(selectedAnswer){
+        setUserAnswers((prevUserAnswers)=>{
+            return[...prevUserAnswers, selectedAnswer]
+        });
+    }
+
+    if (quizIsComplete){
+        return <div id="summary">
+            <img src= {quizCompleteImg} alt="Quiz is Cpmpleted" />
+            <h2> Quiz is Completed</h2>
+        </div>
+    }
+    
+    const shuffledAnswers =[...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=> Math.random-0.5);
+    
+    
+    return(
+        <div id="quiz">
+            <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
+            <ul id="answers">
+                {shuffledAnswers.map((answer)=>
+                    <li key={answer} className="answer">
+                        <button onClick={() =>handleSelectAnswer(answer)}> {answer} </button>
+                    </li>
+                )}
+            </ul>
+        </div>
+        </div>
+        
+        
+
+        
+    );
+}
